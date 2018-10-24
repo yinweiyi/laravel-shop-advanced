9b848209ff0f47a20a0a3eb3de730a791e32b0a4
wayee <378772944@qq.com>
2018-10-24 08:35:42 +0000

    初始化众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,16 +1,24 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Support\Str;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['title', 'description', 'image', 'on_sale', 'rating', 'sold_count', 'review_count', 'price'];
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+    protected $fillable = ['title', 'description', 'image', 'on_sale', 'rating', 'sold_count', 'review_count', 'price', 'type'];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
+
     // 与商品SKU关联
     public function skus()
     {
@@ -29,5 +37,10 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 }
