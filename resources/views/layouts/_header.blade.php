de271c0c37415ccd2eec565d8ccd90dcc81f686b
wayee <378772944@qq.com>
2018-10-24 08:18:24 +0000

    顶部导航加入商品类目树

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#app-navbar-collapse">
                 <span class="sr-only">Toggle Navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -13,6 +14,18 @@
         </div>
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <ul class="nav navbar-nav">
+                <!-- 顶部类目菜单开始 -->
+                <!-- 判断模板是否有 $categoryTree 变量 -->
+                @if(isset($categoryTree))
+                    <li>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">所有类目 <b class="caret"></b></a>
+                        <ul class="dropdown-menu multi-level">
+                            <!-- 遍历 $categoryTree 集合，将集合中的每一项以 $category 变量注入 layouts._category_item 模板中并渲染 -->
+                            @each('layouts._category_item', $categoryTree, 'category')
+                        </ul>
+                    </li>
+            @endif
+            <!-- 顶部类目菜单结束 -->
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <!-- 登录注册链接开始 -->
@@ -21,12 +34,14 @@
                     <li><a href="{{ route('register') }}">注册</a></li>
                 @else
                     <li>
-                        <a href="{{ route('cart.index') }}"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
+                        <a href="{{ route('cart.index') }}"><span class="glyphicon glyphicon-shopping-cart"
+                                                                  aria-hidden="true"></span></a>
                     </li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                                <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                                <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
+                                     class="img-responsive img-circle" width="30px" height="30px">
                             </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
@@ -42,18 +57,19 @@
                             </li>
                             <li>
                                 <a href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
+                                   onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                     退出登录
                                 </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
                                     {{ csrf_field() }}
                                 </form>
                             </li>
                         </ul>
                     </li>
-                @endguest
-                <!-- 登录注册链接结束 -->
+            @endguest
+            <!-- 登录注册链接结束 -->
             </ul>
         </div>
     </div>
