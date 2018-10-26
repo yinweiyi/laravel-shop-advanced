2b8ec01cec4698364449578b672704c2d1df8494
wayee <378772944@qq.com>
2018-10-26 07:19:19 +0000

    计算逾期费

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('cron:finish-crowdfunding')->everyMinute();
+        $schedule->command('cron:calculate-installment-fine')->daily();
     }
 
     /**
