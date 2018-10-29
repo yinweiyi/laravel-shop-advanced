e4cdad9a8765cf02724376b7dd69666add0a990b
wayee <378772944@qq.com>
2018-10-29 07:00:51 +0000

    分面搜索

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -13,16 +13,16 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $page    = $request->input('page', 1);
+        $page = $request->input('page', 1);
         $perPage = 16;
 
         // 构建查询
         $params = [
             'index' => 'products',
-            'type'  => '_doc',
-            'body'  => [
-                'from'  => ($page - 1) * $perPage, // 通过当前页数与每页数量计算偏移值
-                'size'  => $perPage,
+            'type' => '_doc',
+            'body' => [
+                'from' => ($page - 1) * $perPage, // 通过当前页数与每页数量计算偏移值
+                'size' => $perPage,
                 'query' => [
                     'bool' => [
                         'filter' => [
@@ -49,7 +49,7 @@ class ProductsController extends Controller
             if ($category->is_directory) {
                 // 如果是一个父类目，则使用 category_path 来筛选
                 $params['body']['query']['bool']['filter'][] = [
-                    'prefix' => ['category_path' => $category->path.$category->id.'-'],
+                    'prefix' => ['category_path' => $category->path . $category->id . '-'],
                 ];
             } else {
                 // 否则直接通过 category_id 筛选
@@ -66,7 +66,7 @@ class ProductsController extends Controller
             foreach ($keywords as $keyword) {
                 $params['body']['query']['bool']['must'][] = [
                     'multi_match' => [
-                        'query'  => $keyword,
+                        'query' => $keyword,
                         'fields' => [
                             'title^2',
                             'long_title^2',
@@ -75,6 +75,59 @@ class ProductsController extends Controller
                             'skus_title',
                             'skus_description',
                             'properties_value',
+                        ],
+                    ],
+                ];
+            }
+        }
+
+        // 只有当用户有输入搜索词或者使用了类目筛选的时候才会做聚合
+        if ($search || isset($category)) {
+            $params['body']['aggs'] = [
+                'properties' => [
+                    'nested' => [
+                        'path' => 'properties',
+                    ],
+                    'aggs' => [
+                        'properties' => [
+                            'terms' => [
+                                'field' => 'properties.name',
+                            ],
+                            'aggs' => [
+                                'value' => [
+                                    'terms' => [
+                                        'field' => 'properties.value',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ];
+        }
+
+        // 从用户请求参数获取 filters
+        $propertyFilters = [];
+        if ($filterString = $request->input('filters')) {
+            // 将获取到的字符串用符号 | 拆分成数组
+            $filterArray = explode('|', $filterString);
+
+
+            foreach ($filterArray as $filter) {
+                // 将字符串用符号 : 拆分成两部分并且分别赋值给 $name 和 $value 两个变量
+                list($name, $value) = explode(':', $filter);
+                // 将用户筛选的属性添加到数组中
+                $propertyFilters[$name] = $value;
+
+                // 添加到 filter 类型中
+                $params['body']['query']['bool']['filter'][] = [
+                    // 由于我们要筛选的是 nested 类型下的属性，因此需要用 nested 查询
+                    'nested' => [
+                        // 指明 nested 字段
+                        'path' => 'properties',
+                        'query' => [
+                            ['term' => ['properties.name' => $name]],
+                            ['term' => ['properties.value' => $value]],
                         ],
                     ],
                 ];
@@ -96,13 +149,33 @@ class ProductsController extends Controller
             'path' => route('products.index', false), // 手动构建分页的 url
         ]);
 
+        $properties = [];
+        // 如果返回结果里有 aggregations 字段，说明做了分面搜索
+        if (isset($result['aggregations'])) {
+            // 使用 collect 函数将返回值转为集合
+            $properties = collect($result['aggregations']['properties']['properties']['buckets'])
+                ->map(function ($bucket) {
+                    // 通过 map 方法取出我们需要的字段
+                    return [
+                        'key' => $bucket['key'],
+                        'values' => collect($bucket['value']['buckets'])->pluck('key')->all(),
+                    ];
+                })->filter(function ($property) use ($propertyFilters) {
+                    // 过滤掉只剩下一个值 或者 已经在筛选条件里的属性
+                    return count($property['values']) > 1 && !isset($propertyFilters[$property['key']]);
+                });;
+        }
+
+
         return view('products.index', [
             'products' => $pager,
-            'filters'  => [
+            'filters' => [
                 'search' => '',
-                'order'  => $order,
+                'order' => $order,
             ],
             'category' => $category ?? null,
+            'properties' => $properties,
+            'propertyFilters' => $propertyFilters,
         ]);
     }
 
@@ -114,20 +187,20 @@ class ProductsController extends Controller
 
         $favored = false;
         // 用户未登录时返回的是 null，已登录时返回的是对应的用户对象
-        if($user = $request->user()) {
+        if ($user = $request->user()) {
             // 从当前用户已收藏的商品中搜索 id 为当前商品 id 的商品
             // boolval() 函数用于把值转为布尔值
             $favored = boolval($user->favoriteProducts()->find($product->id));
         }
-        
+
         $reviews = OrderItem::query()
-            ->with(['order.user', 'productSku']) // 预先加载关联关系
+            ->with(['order.user', 'productSku'])// 预先加载关联关系
             ->where('product_id', $product->id)
-            ->whereNotNull('reviewed_at') // 筛选出已评价的
-            ->orderBy('reviewed_at', 'desc') // 按评价时间倒序
-            ->limit(10) // 取出 10 条
+            ->whereNotNull('reviewed_at')// 筛选出已评价的
+            ->orderBy('reviewed_at', 'desc')// 按评价时间倒序
+            ->limit(10)// 取出 10 条
             ->get();
-        
+
         // 最后别忘了注入到模板中
         return view('products.show', [
             'product' => $product,
